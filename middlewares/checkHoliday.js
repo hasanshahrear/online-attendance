30114239e800e0718b012697db932583748bfea4
Hasan Shahrear <hasanshahrear143@gmail.com>
2023-06-17 20:42:11 +0600

    Leave message update

--- middlewares/checkHoliday.js
+++ middlewares/checkHoliday.js
@@ -9,7 +9,7 @@ const HTTP_SERVER_ERROR = 500;
 // checkWeeklyHoliday Gard
 const checkHoliday = async (req, res, next)=>{
     const holiday = await IsHolidayCheck.findOne().exec();
-    if(!holiday.holyDay){
+    if(!holiday?.holyDay){
         next();
     }else{
         res.status(HTTP_OK).json({
