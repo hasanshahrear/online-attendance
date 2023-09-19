3ca75d4e05995fa596cf5cedd9ac2b925583381f
Maruf H <maruf.hossaiin@gmail.com>
2023-09-19 20:25:36 +0600

    update holiday api, delete api, get by id and get all api

--- router/useAddHolidays.js
+++ router/useAddHolidays.js
@@ -2,12 +2,17 @@
 const express = require("express")
 
 // internal imports
-const {addHoliday, getAllHolidays} = require("../controller/holidays.controller")
+const {addHoliday, getAllHolidays, getAllHolidayList, getHolidayById, updateHoliday, deleteHoliday} = require("../controller/holidays.controller")
 const { checkAdminLogin } = require("../middlewares/checkLogin")
 
 const router = express.Router()
 
 router.post("/", checkAdminLogin, addHoliday)
 router.get("/", checkAdminLogin, getAllHolidays)
+//new
+router.get("/list", checkAdminLogin, getAllHolidayList)
+router.get("/:id", checkAdminLogin, getHolidayById)
+router.put("/:id", checkAdminLogin, updateHoliday)
+router.delete("/:id", checkAdminLogin, deleteHoliday)
 
 module.exports = router
